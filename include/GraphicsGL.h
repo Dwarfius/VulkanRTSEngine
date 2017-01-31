d8e97145392769e820cfa0e8ac46e2749d2f5d8b
Dwarfius <youman6@gmail.com>
2017-01-31 12:23:07 +0000

    Added uniform definitions to render job, haven't tested yet

--- include/GraphicsGL.h
+++ include/GraphicsGL.h
@@ -28,10 +28,26 @@ private:
 
 	// emulating a render queue
 	// should cache the shader's uniforms as well
+	enum UniformType { Int, Float, Vec2, Vec3, Vec4, Mat4 };
+	
+	struct Uniform {
+		UniformType type;
+		union Value {
+			int32_t i;
+			float f;
+			vec2 v2;
+			vec3 v3;
+			vec4 v4;
+			mat4 m;
+		} value;
+	};
+	
 	struct RenderJob {
 		ShaderId shader;
 		TextureId texture;
 		ModelId model;
+		
+		map<GLuint, Uniform> uniforms;
 	};
 	// supporting max 16 threads
 	// might want to multi-buffer this
