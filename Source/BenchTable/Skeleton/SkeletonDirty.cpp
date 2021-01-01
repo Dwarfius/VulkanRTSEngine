b1f39912fa688f64546d4987251d02ab16c433ca
Dwarfius <youman6@gmail.com>
2021-01-01 22:20:24 +0000

    Update: Improved named for SkeletonDirty set of benchmarks Update: Got rid of extra memory indirection to refine the SkeletonDirty benchmarks

--- Source/BenchTable/Skeleton/SkeletonDirty.cpp
+++ Source/BenchTable/Skeleton/SkeletonDirty.cpp
@@ -7,22 +7,32 @@
 // to group together bones when trying to dirty the hierarchy
 // Results: Grouping and PMR-using iteratation scales better than
 // recursive or ungrouped iterations - on 1024 bones with 10% to dirty
-// it was 3 times faster, while on smaller sets (128 bones) it had very
-// similar performance
+// PMR was 3 times faster than Dirty_Ungrouped, and 2 time faster than Dirty_Grouped
+// while on smaller sets (128 bones) it had very similar performance
 
-struct DirtyEntry
+template<class T>
+struct DirtyTemplate
 {
+	T myBone;
 	bool myIsDirty;
 };
 
-static void GroupedPMR(benchmark::State& aState)
+static void Dirty_GroupedPMR(benchmark::State& aState)
 {
-	std::vector<GroupedBone> bones = GroupedBone::ProcessBones(kBoneInputs, aState.range(0));
+	using DirtyBone = DirtyTemplate<GroupedBone>;
 	const int64_t dirtyCount = (int64_t)(aState.range(0) * 0.1);
-	std::vector<DirtyEntry> dirtyEntries;
-	dirtyEntries.resize(bones.size());
+	std::vector<DirtyBone> bones;
 
-	// TODO: bench adding an early out here, before adding to the queue!
+	{
+		std::vector<GroupedBone> origBones = GroupedBone::ProcessBones(kBoneInputs, aState.range(0));
+		bones.resize(origBones.size());
+		for (size_t i = 0; i < origBones.size(); i++)
+		{
+			bones[i].myBone = origBones[i];
+			bones[i].myIsDirty = false;
+		}
+	}
+
 	constexpr size_t kBonesMemorySize = 512;
 	Skeleton::BoneIndex bonesMemory[kBonesMemorySize];
 	std::pmr::monotonic_buffer_resource stackRes(bonesMemory, sizeof(bonesMemory));
@@ -31,7 +41,10 @@ static void GroupedPMR(benchmark::State& aState)
 	for (auto _ : aState)
 	{
 		aState.PauseTiming();
-		std::memset(dirtyEntries.data(), 0, dirtyEntries.size() * sizeof(DirtyEntry));
+		for (DirtyBone& bone : bones)
+		{
+			bone.myIsDirty = false;
+		}
 		aState.ResumeTiming();
 
 		// dirty all bones
@@ -46,101 +59,128 @@ static void GroupedPMR(benchmark::State& aState)
 			const Skeleton::BoneIndex boneIndex = bonesToVisit.front();
 			bonesToVisit.pop_back();
 
-			DirtyEntry& entry = dirtyEntries[boneIndex];
-			if (entry.myIsDirty)
+			DirtyBone& bone = bones[boneIndex];
+			if (bone.myIsDirty)
 			{
 				// the hierarchy bellow has already been dirtied
 				// so skip this one
 				continue;
 			}
 			// dirty current bone
-			entry.myIsDirty = true;
+			bone.myIsDirty = true;
 
-			const GroupedBone& bone = bones[boneIndex];
-			ASSERT_STR(bonesToVisit.size() + bone.myChildCount < kBonesMemorySize,
+			ASSERT_STR(bonesToVisit.size() + bone.myBone.myChildCount < kBonesMemorySize,
 				"Not enough space allocated on the stack to dirty "
 				"the hierarchy! Try increasing the size!");
 
 			// and schedule all of the children for dirtying
 			for (Skeleton::BoneIndex childOffset = 0;
-				childOffset < bone.myChildCount;
+				childOffset < bone.myBone.myChildCount;
 				childOffset++)
 			{
-				bonesToVisit.push_front(bone.myFirstChildInd + childOffset);
+				bonesToVisit.push_front(bone.myBone.myFirstChildInd + childOffset);
 			}
 		}
 	}
 }
 
-static void Dirty(const std::vector<GroupedBone>& aBones, std::vector<DirtyEntry>& aEntries, Skeleton::BoneIndex aIndex)
+static void Dirty(std::vector<DirtyTemplate<GroupedBone>>& aBones, Skeleton::BoneIndex aIndex)
 {
-	if (aEntries[aIndex].myIsDirty)
+	using DirtyBone = DirtyTemplate<GroupedBone>;
+
+	if (aBones[aIndex].myIsDirty)
 	{
 		return;
 	}
-	aEntries[aIndex].myIsDirty = true;
+	aBones[aIndex].myIsDirty = true;
 
-	const GroupedBone& bone = aBones[aIndex];
+	DirtyBone& bone = aBones[aIndex];
 	for (Skeleton::BoneIndex childOffset = 0;
-		childOffset < bone.myChildCount;
+		childOffset < bone.myBone.myChildCount;
 		childOffset++)
 	{
-		Dirty(aBones, aEntries, bone.myFirstChildInd + childOffset);
+		Dirty(aBones, bone.myBone.myFirstChildInd + childOffset);
 	}
 }
 
-static void Grouped(benchmark::State& aState)
+static void Dirty_Grouped(benchmark::State& aState)
 {
-	std::vector<GroupedBone> bones = GroupedBone::ProcessBones(kBoneInputs, aState.range(0));
+	using DirtyBone = DirtyTemplate<GroupedBone>;
 	const int64_t dirtyCount = (int64_t)(aState.range(0) * 0.1);
-	std::vector<DirtyEntry> dirtyEntries;
-	dirtyEntries.resize(bones.size());
+	std::vector<DirtyBone> bones;
+
+	{
+		std::vector<GroupedBone> origBones = GroupedBone::ProcessBones(kBoneInputs, aState.range(0));
+		bones.resize(origBones.size());
+		for (size_t i = 0; i < origBones.size(); i++)
+		{
+			bones[i].myBone = origBones[i];
+			bones[i].myIsDirty = false;
+		}
+	}
+
 	for (auto _ : aState)
 	{
 		aState.PauseTiming();
-		std::memset(dirtyEntries.data(), 0, dirtyEntries.size() * sizeof(DirtyEntry));
+		for (DirtyBone& bone : bones)
+		{
+			bone.myIsDirty = false;
+		}
 		aState.ResumeTiming();
 
 		// dirty all bones
 		for (int64_t i = 0; i < dirtyCount; i++)
 		{
 			Skeleton::BoneIndex boneToDirty = kIndices[i];
-			Dirty(bones, dirtyEntries, boneToDirty);
+			Dirty(bones, boneToDirty);
 		}
 	}
 }
 
-static void UnGrouped(benchmark::State& aState)
+static void Dirty_UnGrouped(benchmark::State& aState)
 {
-	std::vector<Bone> bones = Bone::ProcessBones(kBoneInputs, aState.range(0));
+	using DirtyBone = DirtyTemplate<Bone>;
 	const int64_t dirtyCount = (int64_t)(aState.range(0) * 0.1);
-	std::vector<DirtyEntry> dirtyEntries;
-	dirtyEntries.resize(bones.size());
+	std::vector<DirtyBone> bones;
+
+	{
+		std::vector<Bone> origBones = Bone::ProcessBones(kBoneInputs, aState.range(0));
+		bones.resize(origBones.size());
+		for (size_t i = 0; i < origBones.size(); i++)
+		{
+			bones[i].myBone = origBones[i];
+			bones[i].myIsDirty = false;
+		}
+	}
+
 	for (auto _ : aState)
 	{
 		aState.PauseTiming();
-		std::memset(dirtyEntries.data(), 0, dirtyEntries.size() * sizeof(DirtyEntry));
+		for(DirtyBone& bone : bones)
+		{
+			bone.myIsDirty = false;
+		}
 		aState.ResumeTiming();
 
 		// dirty all bones
 		for (int64_t i = 0; i < dirtyCount; i++)
 		{
 			Skeleton::BoneIndex boneToDirty = kIndices[i];
-			dirtyEntries[boneToDirty].myIsDirty = true;
+			bones[boneToDirty].myIsDirty = true;
 		}
 
 		// mark the hierarchy for dirty
 		for (int64_t i = 0; i < bones.size(); i++)
 		{
-			const Bone& bone = bones[i];
-			if (i != 0 && dirtyEntries[bone.myParentInd].myIsDirty)
+			const DirtyBone& bone = bones[i];
+			if (i != 0 && bones[bone.myBone.myParentInd].myIsDirty)
 			{
-				dirtyEntries[i].myIsDirty = true;
+				bones[i].myIsDirty = true;
 			}
 		}
 	}
 }
 
-BENCHMARK(GroupedPMR)->Arg(32)->Arg(64)->Arg(128)->Arg(512)->Arg(1024);;
-BENCHMARK(Grouped)->Arg(32)->Arg(64)->Arg(128)->Arg(512)->Arg(1024);;
-BENCHMARK(UnGrouped)->Arg(32)->Arg(64)->Arg(128)->Arg(512)->Arg(1024);;
+BENCHMARK(Dirty_GroupedPMR)->Arg(32)->Arg(64)->Arg(128)->Arg(512)->Arg(1024);;
+BENCHMARK(Dirty_Grouped)->Arg(32)->Arg(64)->Arg(128)->Arg(512)->Arg(1024);;
+BENCHMARK(Dirty_UnGrouped)->Arg(32)->Arg(64)->Arg(128)->Arg(512)->Arg(1024);;
