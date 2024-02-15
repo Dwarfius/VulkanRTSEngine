fc943605f989a95185d9b660d7ad8e3a442c6ad1
Dwarfius <youman6@gmail.com>
2024-02-15 23:43:26 +0000

    Update: AssertRWMutex is now much slimmer From 56 bytes down to 16 bytes(technically 9, but alignment). Also removed a rogue header include.

--- Source/Core/Threading/AssertRWMutex.cpp
+++ Source/Core/Threading/AssertRWMutex.cpp
@@ -5,24 +5,20 @@
 
 #include <sstream>
 
-void AssertRWMutex::LockWrite(std::source_location aLocation)
+void AssertRWMutex::LockWrite(const LockState& aLock)
 {
 	// we set these first so that they are available in case of detected contention
-	myWriteFuncName.store(aLocation.function_name(), std::memory_order::relaxed);
-	myWriteFileName.store(aLocation.file_name(), std::memory_order::relaxed);
-	myWriteLine.store(aLocation.line(), std::memory_order::relaxed);
+	const LockState* lastState = myLastLock.exchange(&aLock, std::memory_order::relaxed);
 	std::atomic_thread_fence(std::memory_order::acq_rel);
-	const char* funcName = myReadFuncName.load(std::memory_order::relaxed);
-	const char* fileName = myReadFileName.load(std::memory_order::relaxed);
-	uint_least32_t line = myReadLine.load(std::memory_order::relaxed);
+	
 	// most-significant bit is reserved for a write lock
 	// check whether we just locked a write-locked mutex
 	unsigned char currVal = myLock.fetch_or(kWriteVal);
 	// if any of the bits are set, then a reader write/read-lock is active
 	ASSERT_STR(currVal == 0, "Contention! {} at {}:{} tried to lock mutex for write "
 		"while it's locked for read by {} at {}:{}",
-		aLocation.function_name(), aLocation.file_name(), aLocation.line(),
-		funcName, fileName, line
+		aLock.myLoc.function_name(), aLock.myLoc.file_name(), aLock.myLoc.line(),
+		lastState->myLoc.function_name(), lastState->myLoc.file_name(), lastState->myLoc.line()
 	);
 }
 
@@ -30,42 +26,33 @@ void AssertRWMutex::UnlockWrite()
 {
 	myLock.fetch_xor(kWriteVal);
 	{
-		myWriteFuncName.store("", std::memory_order::relaxed);
-		myWriteFileName.store("", std::memory_order::relaxed);
-		myWriteLine.store(0, std::memory_order::relaxed);
+		myLastLock.store(nullptr, std::memory_order::relaxed);
 		std::atomic_thread_fence(std::memory_order::release);
 	}
 }
 
-void AssertRWMutex::LockRead(std::source_location aLocation)
+void AssertRWMutex::LockRead(const LockState& aLock)
 {
 	// we set these first  so that they are available in case of detected contention
-	myReadFuncName.store(aLocation.function_name(), std::memory_order::relaxed);
-	myReadFileName.store(aLocation.file_name(), std::memory_order::relaxed);
-	myReadLine.store(aLocation.line(), std::memory_order::relaxed);
+	const LockState* lastState = myLastLock.exchange(&aLock, std::memory_order::relaxed);
 	std::atomic_thread_fence(std::memory_order::acq_rel);
-	const char* funcName = myWriteFuncName.load(std::memory_order::relaxed);
-	const char* fileName = myWriteFileName.load(std::memory_order::relaxed);
-	uint_least32_t line = myWriteLine.load(std::memory_order::relaxed);
 	// check whether we've just read-locked a write-locked mutex
 	unsigned char currVal = myLock.fetch_add(1);
 	ASSERT_STR((currVal & kWriteVal) == 0, "Contention! {} at {}:{} tried to lock mutex for read "
 		"while it's locked for write by {} at {}:{}",
-		aLocation.function_name(), aLocation.file_name(), aLocation.line(),
-		funcName, fileName, line
+		aLock.myLoc.function_name(), aLock.myLoc.file_name(), aLock.myLoc.line(),
+		lastState->myLoc.function_name(), lastState->myLoc.file_name(), lastState->myLoc.line()
 	);
 }
 
 void AssertRWMutex::UnlockRead()
 {
-	uint8_t oldVal = myLock.fetch_sub(1);
+	const uint8_t oldVal = myLock.fetch_sub(1);
 
 	// We only reset the last location if there are no more reads
 	if(oldVal == 1)
 	{
-		myReadFuncName.store("", std::memory_order::relaxed);
-		myReadFileName.store("", std::memory_order::relaxed);
-		myReadLine.store(0, std::memory_order::relaxed);
+		myLastLock.store(nullptr, std::memory_order::relaxed);
 		std::atomic_thread_fence(std::memory_order::release);
 	}
 }
@@ -73,9 +60,9 @@ void AssertRWMutex::UnlockRead()
 // ========================
 
 AssertWriteLock::AssertWriteLock(AssertRWMutex& anRWMutex, std::source_location aLocation)
-	: myMutex(anRWMutex)
+	: AssertRWMutex::LockState(anRWMutex, aLocation)
 {
-	myMutex.LockWrite(aLocation);
+	myMutex.LockWrite(*this);
 }
 
 AssertWriteLock::~AssertWriteLock()
@@ -86,9 +73,9 @@ AssertWriteLock::~AssertWriteLock()
 // ========================
 
 AssertReadLock::AssertReadLock(AssertRWMutex& anRWMutex, std::source_location aLocation)
-	: myMutex(anRWMutex)
+	: AssertRWMutex::LockState(anRWMutex, aLocation)
 {
-	myMutex.LockRead(aLocation);
+	myMutex.LockRead(*this);
 }
 
 AssertReadLock::~AssertReadLock()
--- Source/Core/Threading/AssertRWMutex.h
+++ Source/Core/Threading/AssertRWMutex.h
@@ -3,7 +3,6 @@
 #ifdef ASSERT_MUTEX
 
 #include <atomic>
-#include <thread>
 #include <source_location>
 
 // A utility mutex that asserts on double write or write-while-read situations. 
@@ -11,47 +10,49 @@
 class AssertRWMutex
 {
 public:
-	void LockWrite(std::source_location aLocation);
+	class LockState
+	{
+	public:
+		LockState(AssertRWMutex& aMutex, const std::source_location& aLoc)
+			: myMutex(aMutex)
+			, myLoc(aLoc)
+		{
+		}
+
+		AssertRWMutex& myMutex;
+		std::source_location myLoc;
+	};
+
+public:
+	void LockWrite(const LockState& aLock);
 	void UnlockWrite();
 
-	void LockRead(std::source_location aLocation);
+	void LockRead(const LockState& aLock);
 	void UnlockRead();
 
 private:
 	constexpr static uint8_t kWriteVal = 1 << 7;
 
+	std::atomic<const LockState*> myLastLock = nullptr;
 	std::atomic<uint8_t> myLock = 0;
-	// TODO: reduce the footprint of this monstrosity
-	std::atomic<uint_least32_t> myWriteLine = 0;
-	std::atomic<const char*> myWriteFuncName = "";
-	std::atomic<const char*> myWriteFileName = "";
-	std::atomic<uint_least32_t> myReadLine = 0;
-	std::atomic<const char*> myReadFuncName = "";
-	std::atomic<const char*> myReadFileName = "";
 };
 
 // A locker utility that implements RAII on AssertRWMutex for read
-class AssertWriteLock
+class AssertWriteLock : private AssertRWMutex::LockState
 {
 public:
 	AssertWriteLock(AssertRWMutex& anRWMutex, 
 		std::source_location aLocation = std::source_location::current());
 	~AssertWriteLock();
-
-private:
-	AssertRWMutex& myMutex;
 };
 
 // A locker utility that implements RAII on AssertRWMutex for read
-class AssertReadLock
+class AssertReadLock : private AssertRWMutex::LockState
 {
 public:
 	AssertReadLock(AssertRWMutex& anRWMutex,
 		std::source_location aLocation = std::source_location::current());
 	~AssertReadLock();
-
-private:
-	AssertRWMutex& myMutex;
 };
 
 #endif // ASSERT_MUTEX
