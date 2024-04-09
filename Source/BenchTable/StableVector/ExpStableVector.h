816031420325a1c8271eaba726e378ad6a3c6daa
Dwarfius <youman6@gmail.com>
2024-04-09 01:10:55 +0100

    New: adding StableVector Benchtable Going to be my playground to see if I can optimize StableVector. Right now it can lose quite badly (10x on Allocate, 0.2x on ForEach, 2x on ParallelForEach)

---
+++ Source/BenchTable/StableVector/ExpStableVector.h
@@ -0,0 +1,403 @@
+#pragma once
+
+#include <variant>
+
+namespace Exp
+{
+    template<class T, size_t PageSize = 256>
+    class StableVector
+    {
+        struct Page
+        {
+            using FreeListNode = std::variant<size_t, T>;
+            constexpr static uint8_t kSlotIndex = 0;
+            constexpr static uint8_t kValueIndex = 1;
+
+            // Note: A convenience stopgap to help with ParallelFor
+            // Not intended to be a proper iterator implementation, so
+            // before exposing to users it needs being rounded up
+            struct Iterator
+            {
+                T& operator*() const
+                {
+                    return std::get<kValueIndex>(myPage->myItems[myIndex]);
+                }
+
+                void operator++(int)
+                {
+                    do
+                    {
+                        myIndex++;
+                    } while (myIndex < PageSize
+                        && !IsValid());
+                }
+
+                bool operator==(const Iterator& aOther) const
+                {
+                    return myPage == aOther.myPage
+                        && myIndex == aOther.myIndex;
+                }
+
+                bool IsValid() const
+                {
+                    return myPage->myItems[myIndex].index() == kValueIndex;
+                }
+
+                Page* myPage;
+                size_t myIndex;
+            };
+
+            Page()
+            {
+                Clear();
+            }
+
+            template<class... Ts>
+            [[nodiscard]] T& Allocate(Ts&& ...anArgs)
+            {
+                myCount++;
+                FreeListNode& node = myItems[myFreeNode];
+                myFreeNode = std::get<kSlotIndex>(node);
+                return node.emplace<T>(std::forward<Ts>(anArgs)...);
+            }
+
+            void Free(T& anItem)
+            {
+                const char* const itemAddr = reinterpret_cast<const char*>(&anItem);
+                const char* const pageStart = reinterpret_cast<const char*>(std::begin(myItems));
+                const size_t offset = itemAddr - pageStart;
+                const size_t index = offset / sizeof(FreeListNode);
+
+                FreeListNode& node = myItems[index];
+                node = myFreeNode;
+                myFreeNode = index;
+                myCount--;
+            }
+
+            void Clear()
+            {
+                for (size_t i = 0; i < PageSize; i++)
+                {
+                    myItems[i] = i + 1;
+                }
+                myItems[PageSize - 1] = static_cast<size_t>(-1ll);
+                myFreeNode = 0;
+                myCount = 0;
+            }
+
+            [[nodiscard]] bool HasSpace() const
+            {
+                return myFreeNode != static_cast<size_t>(-1ll);
+            }
+
+            [[nodiscard]] bool Contains(const T* anItem) const
+            {
+                const void* itemAddr = anItem;
+                const void* pageStart = std::begin(myItems);
+                const void* pageEnd = std::end(myItems);
+                return pageStart <= itemAddr && itemAddr < pageEnd;
+            }
+
+            template<class TFunc>
+            void ForEach(const TFunc& aFunc)
+            {
+                for (FreeListNode& node : myItems)
+                {
+                    if (node.index() == kValueIndex)
+                    {
+                        aFunc(std::get<kValueIndex>(node));
+                    }
+                }
+            }
+
+            template<class TFunc>
+            void ForEach(const TFunc& aFunc) const
+            {
+                for (const FreeListNode& node : myItems)
+                {
+                    if (node.index() == kValueIndex)
+                    {
+                        aFunc(std::get<kValueIndex>(node));
+                    }
+                }
+            }
+
+            Iterator begin()
+            {
+                Iterator iter{ this, 0 };
+                if (!iter.IsValid())
+                {
+                    iter++;
+                }
+                return iter;
+            }
+            Iterator begin() const
+            {
+                Iterator iter{ this, 0 };
+                if (!iter.IsValid())
+                {
+                    iter++;
+                }
+                return iter;
+            }
+
+            Iterator end() { return { this, PageSize }; }
+            Iterator end() const { return { this, PageSize }; }
+
+            // Note: linear complexity!
+            Iterator GetIterAt(size_t anElemNum)
+            {
+                Iterator iter = begin();
+                while (anElemNum-- > 0)
+                {
+                    iter++;
+                }
+                return iter;
+            }
+
+            FreeListNode myItems[PageSize];
+            size_t myFreeNode = 0;
+            size_t myCount = 0;
+        };
+
+        struct PageNode
+        {
+            Page myPage;
+            PageNode* myNext = nullptr;
+        };
+
+        // Note: A convenience stopgap to help with ParallelFor
+        // Not intended to be a proper iterator implementation, so
+        // before exposing to users it needs being rounded up
+        struct Iterator
+        {
+            T& operator*()
+            {
+                return *myPageIter;
+            }
+
+            void operator++(int)
+            {
+                do
+                {
+                    myPageIter++;
+                    if (myPageIter == myPageNode->myPage.end())
+                    {
+                        myPageNode = myPageNode->myNext;
+                        if (myPageNode)
+                        {
+                            myPageIter = myPageNode->myPage.begin();
+                        }
+                    }
+                } while (myPageNode != nullptr
+                    && myPageIter != myPageNode->myPage.end()
+                    && !myPageIter.IsValid());
+            }
+
+            bool operator==(const Iterator& aOther) const
+            {
+                return myPageNode == aOther.myPageNode
+                    && myPageIter == aOther.myPageIter;
+            }
+
+            PageNode* myPageNode;
+            Page::Iterator myPageIter;
+        };
+
+    public:
+        StableVector() = default;
+        ~StableVector()
+        {
+            PageNode* node = myStartPage.myNext;
+            while (node)
+            {
+                PageNode* currNode = node;
+                node = currNode->myNext;
+                delete currNode;
+            }
+        }
+
+        // Copy constructors can't be supported in principle
+        StableVector(const StableVector&) = delete;
+        StableVector& operator=(const StableVector&) = delete;
+
+        // Move constructors can, but needs a slightly different impl
+        // for storing first page - so not implemented for now
+        StableVector(StableVector&&) = delete;
+        StableVector& operator=(StableVector&&) = delete;
+
+        template<class... Ts>
+        [[nodiscard]] T& Allocate(Ts&& ...anArgs)
+        {
+            Page* freePage = nullptr;
+            for (PageNode* pageNode = &myStartPage; pageNode; pageNode = pageNode->myNext)
+            {
+                Page& page = pageNode->myPage;
+                if (page.HasSpace())
+                {
+                    freePage = &page;
+                    break;
+                }
+                else if (!pageNode->myNext)
+                {
+                    pageNode->myNext = new PageNode;
+                    freePage = &pageNode->myNext->myPage;
+                    break;
+                }
+            }
+            myCount++;
+            return freePage->Allocate(std::forward<Ts>(anArgs)...);
+        }
+
+        void Free(T& anItem)
+        {
+            for (PageNode* pageNode = &myStartPage; pageNode; pageNode = pageNode->myNext)
+            {
+                Page& page = pageNode->myPage;
+                if (page.Contains(&anItem))
+                {
+                    page.Free(anItem);
+                    myCount--;
+                    return;
+                }
+            }
+        }
+
+        void Clear()
+        {
+            for (PageNode* pageNode = &myStartPage; pageNode; pageNode = pageNode->myNext)
+            {
+                Page& page = pageNode->myPage;
+                page.Clear();
+            }
+            myCount = 0;
+        }
+
+        bool IsEmpty() const
+        {
+            return myCount == 0;
+        }
+
+        bool Contains(const T* anItem) const
+        {
+            if (IsEmpty())
+            {
+                return false;
+            }
+
+            for (const PageNode* pageNode = &myStartPage; pageNode; pageNode = pageNode->myNext)
+            {
+                const Page& page = pageNode->myPage;
+                if (page.Contains(anItem))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        template<class TFunc>
+        void ForEach(const TFunc& aFunc)
+        {
+            if (IsEmpty())
+            {
+                return;
+            }
+
+            for (PageNode* pageNode = &myStartPage; pageNode; pageNode = pageNode->myNext)
+            {
+                Page& page = pageNode->myPage;
+                page.ForEach(aFunc);
+            }
+        }
+
+        template<class TFunc>
+        void ForEach(const TFunc& aFunc) const
+        {
+            if (IsEmpty())
+            {
+                return;
+            }
+
+            for (PageNode* pageNode = &myStartPage; pageNode; pageNode = pageNode->myNext)
+            {
+                Page& page = pageNode->myPage;
+                page.ForEach(aFunc);
+            }
+        }
+
+        template<class TFunc>
+        void ParallelForEach(const TFunc& aFunc)
+        {
+            if (IsEmpty())
+            {
+                return;
+            }
+
+            DEBUG_ONLY(std::atomic<size_t> foundCount = 0;);
+            // Attempt to have 2 batches per thread, so that it's easier to schedule around
+            const size_t batchSize = std::max(myCount / std::thread::hardware_concurrency() / 2, 1ull);
+            tbb::parallel_for(tbb::blocked_range<size_t>(0, myCount, batchSize),
+                [&](tbb::blocked_range<size_t> aRange)
+            {
+                // Find starting iterator
+                size_t index = aRange.begin();
+                PageNode* node = &myStartPage;
+                while (index >= node->myPage.myCount)
+                {
+                    index -= node->myPage.myCount;
+                    node = node->myNext;
+                }
+
+                // Iterate over the chunk
+                Iterator iter{ node, node->myPage.GetIterAt(index) };
+                for (size_t i = 0; i < aRange.size(); i++)
+                {
+                    aFunc(*iter);
+                    iter++;
+                    DEBUG_ONLY(foundCount++;);
+                }
+            });
+            ASSERT_STR(foundCount == myCount, "Weird behavior, failed to find all elements!");
+        }
+
+        template<class TFunc>
+        void ParallelForEach(const TFunc& aFunc) const
+        {
+            if (IsEmpty())
+            {
+                return;
+            }
+
+            DEBUG_ONLY(std::atomic<size_t> foundCount = 0;);
+            // Attempt to have 2 batches per thread, so that it's easier to schedule around
+            const size_t batchSize = std::max(myCount / std::thread::hardware_concurrency() / 2, 1ull);
+            tbb::parallel_for(tbb::blocked_range<size_t>(0, myCount, batchSize),
+                [&](tbb::blocked_range<size_t> aRange)
+            {
+                // Find starting iterator
+                size_t index = aRange.begin();
+                PageNode* node = &myStartPage;
+                while (index >= node->myPage.myCount)
+                {
+                    index -= node->myPage.myCount;
+                    node = node->myNext;
+                }
+
+                // Iterate over the chunk
+                Iterator iter{ node, node->myPage.GetIterAt(index) };
+                for (size_t i = 0; i < aRange.size(); i++)
+                {
+                    aFunc(*iter);
+                    iter++;
+                    DEBUG_ONLY(foundCount++;);
+                }
+            });
+            ASSERT_STR(foundCount == myCount, "Weird behavior, failed to find all elements!");
+        }
+
+    private:
+        PageNode myStartPage;
+        size_t myCount = 0;
+    };
+}
---
+++ Source/BenchTable/StableVector/StableVector.cpp
@@ -0,0 +1,213 @@
+#include "Precomp.h"
+
+#include "ExpStableVector.h"
+#include <Core/StableVector.h>
+
+// This set of benchmarks is here to guide the implementation of StableVector in
+// the engine to try to get it as close as possible to Vector while providing 
+// pointer-stability guarantee (so it's not exactly apples to apples). It also 
+// provides a space for Experimental Stable Vector to try out ideas to see if it
+// more can be squeezed out
+
+namespace Impl
+{
+	static void SetupBenchmark(benchmark::internal::Benchmark* aBench)
+	{
+		aBench->RangeMultiplier(4);
+		aBench->Range(64, 4096);
+	}
+
+	static void SetupParallelBenchmark(benchmark::internal::Benchmark* aBench)
+	{
+		aBench->RangeMultiplier(8);
+		aBench->Range(512, 32768); // 32k is enough to show 2x loss
+	}
+}
+
+static void AddToVector(benchmark::State& aState)
+{
+	for (auto _ : aState)
+	{
+		std::vector<uint32_t> vec;
+		for (uint32_t i = 0; i < aState.range(0); i++)
+		{
+			vec.push_back(i);
+		}
+		benchmark::DoNotOptimize(vec.data());
+		benchmark::ClobberMemory();
+	}
+}
+
+static void AddToReservedVector(benchmark::State& aState)
+{
+	for (auto _ : aState)
+	{
+		std::vector<uint32_t> vec;
+		vec.reserve(aState.range(0));
+		for (uint32_t i = 0; i < aState.range(0); i++)
+		{
+			vec.push_back(i);
+		}
+		benchmark::DoNotOptimize(vec.data());
+		benchmark::ClobberMemory();
+	}
+}
+
+static void AddToStableVector(benchmark::State& aState)
+{
+	for (auto _ : aState)
+	{
+		StableVector<uint32_t> vec;
+		uint32_t& firstElem = vec.Allocate(0);
+		for (uint32_t i = 1; i < aState.range(0); i++)
+		{
+			[[maybe_unused]] uint32_t& newElem = vec.Allocate(i);
+		}
+		benchmark::DoNotOptimize(&firstElem);
+		benchmark::ClobberMemory();
+	}
+}
+
+static void AddToExpStableVector(benchmark::State& aState)
+{
+	for (auto _ : aState)
+	{
+		Exp::StableVector<uint32_t> vec;
+		uint32_t& firstElem = vec.Allocate(0);
+		for (uint32_t i = 1; i < aState.range(0); i++)
+		{
+			[[maybe_unused]] uint32_t& newElem = vec.Allocate(i);
+		}
+		benchmark::DoNotOptimize(&firstElem);
+		benchmark::ClobberMemory();
+	}
+}
+
+BENCHMARK(AddToVector)->Apply(Impl::SetupBenchmark);
+BENCHMARK(AddToReservedVector)->Apply(Impl::SetupBenchmark);
+BENCHMARK(AddToStableVector)->Apply(Impl::SetupBenchmark);
+BENCHMARK(AddToExpStableVector)->Apply(Impl::SetupBenchmark);
+
+static void ForEachVector(benchmark::State& aState)
+{
+	std::vector<uint32_t> vec;
+	vec.reserve(aState.range(0));
+	for (uint32_t i = 0; i < aState.range(0); i++)
+	{
+		vec.push_back(i);
+	}
+
+	for (auto _ : aState)
+	{
+		uint32_t sum = 0;
+		for (const uint32_t& elem : vec)
+		{
+			benchmark::DoNotOptimize(sum += elem);
+		}
+	}
+}
+
+static void ForEachStableVector(benchmark::State& aState)
+{
+	StableVector<uint32_t> vec;
+	uint32_t& firstElem = vec.Allocate(0);
+	for (uint32_t i = 1; i < aState.range(0); i++)
+	{
+		[[maybe_unused]] uint32_t& newElem = vec.Allocate(i);
+	}
+
+	for (auto _ : aState)
+	{
+		uint32_t sum = 0;
+		vec.ForEach([&sum](uint32_t anElem) {
+			benchmark::DoNotOptimize(sum += anElem);
+		});
+	}
+}
+
+static void ForEachExpStableVector(benchmark::State& aState)
+{
+	Exp::StableVector<uint32_t> vec;
+	uint32_t& firstElem = vec.Allocate(0);
+	for (uint32_t i = 1; i < aState.range(0); i++)
+	{
+		[[maybe_unused]] uint32_t& newElem = vec.Allocate(i);
+	}
+
+	for (auto _ : aState)
+	{
+		uint32_t sum = 0;
+		vec.ForEach([&sum](uint32_t anElem) {
+			benchmark::DoNotOptimize(sum += anElem);
+		});
+	}
+}
+
+BENCHMARK(ForEachVector)->Apply(Impl::SetupBenchmark);
+BENCHMARK(ForEachStableVector)->Apply(Impl::SetupBenchmark);
+BENCHMARK(ForEachExpStableVector)->Apply(Impl::SetupBenchmark);
+
+static void ParallelForEachVector(benchmark::State& aState)
+{
+	std::vector<uint32_t> vec;
+	vec.reserve(aState.range(0));
+	for (uint32_t i = 0; i < aState.range(0); i++)
+	{
+		vec.push_back(i);
+	}
+
+	for (auto _ : aState)
+	{
+		uint32_t sum = 0;
+		// same as StableVector batching
+		const size_t batchSize = std::max(vec.size() / std::thread::hardware_concurrency() / 2, 1ull);
+		tbb::parallel_for(tbb::blocked_range<size_t>(0, vec.size(), batchSize),
+			[&](tbb::blocked_range<size_t> aRange)
+		{
+			for (size_t i = aRange.begin(); i < aRange.end(); i++)
+			{
+				benchmark::DoNotOptimize(sum += vec[i]);
+			}
+		});
+	}
+}
+
+static void ParallelForEachStableVector(benchmark::State& aState)
+{
+	StableVector<uint32_t> vec;
+	uint32_t& firstElem = vec.Allocate(0);
+	for (uint32_t i = 1; i < aState.range(0); i++)
+	{
+		[[maybe_unused]] uint32_t& newElem = vec.Allocate(i);
+	}
+
+	for (auto _ : aState)
+	{
+		uint32_t sum = 0;
+		vec.ParallelForEach([&sum](uint32_t anElem) {
+			benchmark::DoNotOptimize(sum += anElem);
+		});
+	}
+}
+
+static void ParallelForEachExpStableVector(benchmark::State& aState)
+{
+	Exp::StableVector<uint32_t> vec;
+	uint32_t& firstElem = vec.Allocate(0);
+	for (uint32_t i = 1; i < aState.range(0); i++)
+	{
+		[[maybe_unused]] uint32_t& newElem = vec.Allocate(i);
+	}
+
+	for (auto _ : aState)
+	{
+		uint32_t sum = 0;
+		vec.ParallelForEach([&sum](uint32_t anElem) {
+			benchmark::DoNotOptimize(sum += anElem);
+		});
+	}
+}
+
+BENCHMARK(ParallelForEachVector)->Apply(Impl::SetupParallelBenchmark);
+BENCHMARK(ParallelForEachStableVector)->Apply(Impl::SetupParallelBenchmark);
+BENCHMARK(ParallelForEachExpStableVector)->Apply(Impl::SetupParallelBenchmark);
