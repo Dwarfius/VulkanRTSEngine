9ef8f5b61d2ab5aee8432eff3faab60df860fe64
Dwarfius <youman6@gmail.com>
2024-05-14 20:55:40 +0100

    Update: Preparing Descriptor to be constexpr-friendly This is part of optimization push to shrink FillUBO, as right now it's the most expensive step. I hope I can make Descriptor produce compile-time calculated offsets

--- Source/Graphics/Descriptor.cpp
+++ Source/Graphics/Descriptor.cpp
@@ -1,71 +1,6 @@
 #include "Precomp.h"
 #include "Descriptor.h"
 
-#include <Core/Resources/Serializer.h>
-
-Descriptor::Descriptor(std::initializer_list<InitArgs> aUniformList)
-{
-	for (InitArgs uniform : aUniformList)
-	{
-		AddUniformType(uniform.myType, uniform.myArraySize);
-	}
-	RecomputeSize();
-}
-
-void Descriptor::SetUniformType(uint32_t aSlot, UniformType aType, uint32_t anArraySize)
-{
-	if (aSlot >= myEntries.size())
-	{
-		myEntries.resize(aSlot + 1llu);
-	}
-	myEntries[aSlot] = { 0, anArraySize, aType };
-}
-
-void Descriptor::AddUniformType(UniformType aType /* = UniformType::Int*/, uint32_t anArraySize /* = 1 */)
-{
-	myEntries.push_back({ 0, anArraySize, aType });
-}
-
-void Descriptor::RemoveUniformType(uint32_t aSlot)
-{
-	myEntries.erase(myEntries.begin() + aSlot);
-}
-
-void Descriptor::RecomputeSize()
-{
-	const size_t size = myEntries.size();
-
-	myTotalSize = 0;
-	for (uint32_t i = 0; i < size; i++)
-	{
-		size_t basicAlignment = 0;
-		size_t elemSize = 0;
-		GetSizeAndAlignment(i, elemSize, basicAlignment);
-
-		// myTotalSize has the aligned offset from the previous iter
-		// plus the size of the new element.
-		// calculate the adjusted size (with padding), so that we can
-		// retain alignment required for new element
-		size_t remainder = myTotalSize % basicAlignment;
-		if (remainder)
-		{
-			myTotalSize += basicAlignment - remainder;
-		}
-
-		Entry& entry = myEntries[i];
-		entry.myByteOffset = myTotalSize;
-		myTotalSize += elemSize * entry.myArraySize;
-	}
-
-	// since it's a struct, per std140 rules it has to be 16-aligned
-	constexpr size_t kStructAlignment = 16;
-	size_t remainder = myTotalSize % kStructAlignment;
-	if (remainder)
-	{
-		myTotalSize += kStructAlignment - remainder;
-	}
-}
-
 char* Descriptor::GetPtr(uint32_t aSlot, size_t anArrayIndex, char* aBasePtr) const
 {
 	const Entry& entry = myEntries[aSlot];
@@ -75,39 +10,7 @@ char* Descriptor::GetPtr(uint32_t aSlot, size_t anArrayIndex, char* aBasePtr) co
 	return aBasePtr + entry.myByteOffset + anArrayIndex * slotSize;
 }
 
-size_t Descriptor::GetSlotSize(uint32_t aSlot) const
-{
-	size_t slotSize = 0;
-	size_t ignored;
-	GetSizeAndAlignment(aSlot, slotSize, ignored);
-	return slotSize * myEntries[aSlot].myArraySize;
-}
-
-void Descriptor::GetSizeAndAlignment(uint32_t aSlot, size_t& aSize, size_t& anAlignment) const
-{
-	const Entry& entry = myEntries[aSlot];
-	switch (entry.myUniformType)
-	{
-	case UniformType::Float:
-	case UniformType::Int:
-		// According to spec, arrays of scalars and vectors have
-		// multiple of vec4 size&alignment
-		anAlignment = entry.myArraySize == 1 ? sizeof(int) : sizeof(glm::vec4);
-		aSize = entry.myArraySize == 1 ? sizeof(int) : sizeof(glm::vec4);
-		break;
-	case UniformType::Vec2:
-		anAlignment = entry.myArraySize == 1 ? sizeof(glm::vec2) : sizeof(glm::vec4);
-		aSize = entry.myArraySize == 1 ? sizeof(glm::vec2) : sizeof(glm::vec4);
-		break;
-	case UniformType::Vec4:
-		anAlignment = sizeof(glm::vec4);
-		aSize = sizeof(glm::vec4);
-		break;
-	case UniformType::Mat4:
-		anAlignment = sizeof(glm::vec4);
-		aSize = sizeof(glm::mat4);
-		break;
-	default:
-		ASSERT_STR(false, "Unrecognized Uniform Type found!");
-	}
-}
+static constexpr Descriptor kDesc{
+	{Descriptor::UniformType::Int}
+};
+static_assert(kDesc.GetUniformCount() == 1);
--- Source/Graphics/Descriptor.h
+++ Source/Graphics/Descriptor.h
@@ -1,14 +1,16 @@
 #pragma once
 
 #include <Core/DataEnum.h>
+#include <Core/StaticVector.h>
 
 class Serializer;
 
 // A class that describes how uniforms are stored for passing to the GPU.
-// Follows the std140 layout. Only supports AoS layout.
+// Follows the std140 layout. Only supports AoS layout. 
+// Has capacity for 16 entries!
 // Use example:
 // Descriptor d();
-// d.SetUniformType(0, UniformType::Vec2);
+// d.AddUniformType(UniformType::Vec2, 20);
 // ...
 // d.RecomputeSize();
 // UniformBlock b(2, d);
@@ -38,36 +40,119 @@ private:
 	};
 
 public:
-	Descriptor() = default;
-	Descriptor(std::initializer_list<InitArgs> aUniformsList);
+	constexpr Descriptor() = default;
+	constexpr Descriptor(std::initializer_list<InitArgs> aUniformsList);
 
-	// Marks the slot to be used storing a uniform of specific type
-	void SetUniformType(uint32_t aSlot, UniformType aType, uint32_t anArraySize);
-	void AddUniformType(UniformType aType = UniformType::Int, uint32_t anArraySize = 1);
-	void RemoveUniformType(uint32_t aSlot);
+	constexpr void AddUniformType(UniformType aType = UniformType::Int, uint32_t anArraySize = 1);
 
 	// Goes through the accumulated types and calculates the total size
 	// Follows the std140 layout specification
-	void RecomputeSize();
+	constexpr void RecomputeSize();
 
 	// Returns how many uniforms does this descriptor have
-	size_t GetUniformCount() const { return myEntries.size(); }
+	constexpr size_t GetUniformCount() const { return myEntries.GetSize(); }
 	// Returns the aligned size of the memory block that's 
 	// needed to contain array of all types.
 	// 16-byte alligned according to std140 layour specification
-	size_t GetBlockSize() const { return myTotalSize; }
+	constexpr size_t GetBlockSize() const { return myTotalSize; }
 	// Returns the unaligned offset for a specific slot.
 	// 4-byte alligned according to std140 layour specification
 	char* GetPtr(uint32_t aSlot, size_t anArrayIndex, char* aBasePtr) const;
 	// Returns the type of value stored at the specific slot
-	UniformType GetType(uint32_t aSlot) const { return myEntries[aSlot].myUniformType; }
+	constexpr UniformType GetType(uint32_t aSlot) const { return myEntries[aSlot].myUniformType; }
 
-	size_t GetSlotSize(uint32_t aSlot) const;
-	uint32_t GetArraySize(uint32_t aSlot) const { return myEntries[aSlot].myArraySize; }
+	constexpr size_t GetSlotSize(uint32_t aSlot) const;
+	constexpr uint32_t GetArraySize(uint32_t aSlot) const { return myEntries[aSlot].myArraySize; }
 	
 private:
-	void GetSizeAndAlignment(uint32_t aSlot, size_t& aSize, size_t& anAlignment) const;
+	constexpr void GetSizeAndAlignment(uint32_t aSlot, size_t& aSize, size_t& anAlignment) const;
 
-	std::vector<Entry> myEntries;
+	StaticVector<Entry, 16> myEntries;
 	size_t myTotalSize = 0;
 };
+
+constexpr Descriptor::Descriptor(std::initializer_list<InitArgs> aUniformList)
+{
+	for (InitArgs uniform : aUniformList)
+	{
+		AddUniformType(uniform.myType, uniform.myArraySize);
+	}
+	RecomputeSize();
+}
+
+constexpr void Descriptor::AddUniformType(UniformType aType /* = UniformType::Int*/, uint32_t anArraySize /* = 1 */)
+{
+	myEntries.PushBack({ 0, anArraySize, aType });
+}
+
+constexpr void Descriptor::RecomputeSize()
+{
+	const size_t size = myEntries.GetSize();
+
+	myTotalSize = 0;
+	for (uint32_t i = 0; i < size; i++)
+	{
+		size_t basicAlignment = 0;
+		size_t elemSize = 0;
+		GetSizeAndAlignment(i, elemSize, basicAlignment);
+
+		// myTotalSize has the aligned offset from the previous iter
+		// plus the size of the new element.
+		// calculate the adjusted size (with padding), so that we can
+		// retain alignment required for new element
+		size_t remainder = myTotalSize % basicAlignment;
+		if (remainder)
+		{
+			myTotalSize += basicAlignment - remainder;
+		}
+
+		Entry& entry = myEntries[i];
+		entry.myByteOffset = myTotalSize;
+		myTotalSize += elemSize * entry.myArraySize;
+	}
+
+	// since it's a struct, per std140 rules it has to be 16-aligned
+	constexpr size_t kStructAlignment = 16;
+	size_t remainder = myTotalSize % kStructAlignment;
+	if (remainder)
+	{
+		myTotalSize += kStructAlignment - remainder;
+	}
+}
+
+constexpr size_t Descriptor::GetSlotSize(uint32_t aSlot) const
+{
+	size_t slotSize = 0;
+	size_t ignored;
+	GetSizeAndAlignment(aSlot, slotSize, ignored);
+	return slotSize * myEntries[aSlot].myArraySize;
+}
+
+constexpr void Descriptor::GetSizeAndAlignment(uint32_t aSlot, size_t& aSize, size_t& anAlignment) const
+{
+	const Entry& entry = myEntries[aSlot];
+	switch (entry.myUniformType)
+	{
+	case UniformType::Float:
+	case UniformType::Int:
+		// According to spec, arrays of scalars and vectors have
+		// multiple of vec4 size&alignment
+		anAlignment = entry.myArraySize == 1 ? sizeof(int) : sizeof(glm::vec4);
+		aSize = entry.myArraySize == 1 ? sizeof(int) : sizeof(glm::vec4);
+		break;
+	case UniformType::Vec2:
+		anAlignment = entry.myArraySize == 1 ? sizeof(glm::vec2) : sizeof(glm::vec4);
+		aSize = entry.myArraySize == 1 ? sizeof(glm::vec2) : sizeof(glm::vec4);
+		break;
+	case UniformType::Vec4:
+		anAlignment = sizeof(glm::vec4);
+		aSize = sizeof(glm::vec4);
+		break;
+	case UniformType::Mat4:
+		anAlignment = sizeof(glm::vec4);
+		aSize = sizeof(glm::mat4);
+		break;
+	default:
+		ASSERT_STR(false, "Unrecognized Uniform Type found!");
+	}
+}
