a2cadfba2e506a0dace70597dd23527e544b461a
Dwarfius <youman6@gmail.com>
2024-11-10 21:45:10 +0000

    Clean: dead code removal

--- Source/Engine/Graphics/RenderPasses/GenericRenderPasses.h
+++ Source/Engine/Graphics/RenderPasses/GenericRenderPasses.h
@@ -7,12 +7,6 @@ class DefaultRenderPass final : public RenderPass
 public:
 	constexpr static uint32_t kId = Utils::CRC32("DefaultRenderPass");
 
-	struct Params
-	{
-		uint32_t myOffset = 0; // rendering param, offset into rendering buffer
-		uint32_t myCount = -1; // rendering param, how many elements to render from a buffer, all by default
-	};
-
 	Id GetId() const override { return kId; }
 
 	void Execute(Graphics& aGraphics) override;
@@ -27,11 +21,6 @@ class TerrainRenderPass final : public RenderPass
 {
 public:
 	constexpr static uint32_t kId = Utils::CRC32("TerrainRenderPass");
-
-	struct Params
-	{
-		int myTileCount = 0;
-	};
 
 	TerrainRenderPass();
 
